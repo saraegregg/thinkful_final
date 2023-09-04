fedbaaf754e44dc079aa504bd3cac06464ab3eda
Sara Gregg <sara.gregg@target.com>
2023-09-04 11:25:36 -0500

    passing form tests

--- src/utils/Form.js
+++ src/utils/Form.js
@@ -3,44 +3,87 @@ import CancelButton from '../components/buttons/CancelButton';
 import SubmitButton from '../components/buttons/SubmitButton';
 
 function Form(props) {
-    return (
-        <div>
-            <h2 className="p-2">{props.formTitle}</h2>
-            <form onSubmit={props.submitHandler}>
-                <div className="row">
-                    <div className="col">
-                    <label htmlFor={props.htmlForOne}>{props.formLabelOne}</label>
-                    <textarea
-                        required
-                        rows="4" cols="30"
-                        placeholder={props.formPlaceholderOne}
-                        name={props.formNameOne}
-                        type="text"
-                        onChange={props.changeHandler}
-                        defaultValue={props.formValueOne ? props.formValueOne : ""}
-                    />
+    if (props.formTitle === "Create Deck"){
+        return (
+            <div>
+                <h2 className="p-2">{props.formTitle}</h2>
+                <form onSubmit={props.submitHandler}>
+                    <div className="row">
+                        <div className="col">
+                        <label htmlFor={props.htmlForOne}>{props.formLabelOne}</label>
+                        <input
+                            type="text"
+                            className="textarea"
+                            required
+                            rows="4" cols="30"
+                            placeholder={props.formPlaceholderOne}
+                            name={props.formNameOne}
+                            onChange={props.changeHandler}
+                            defaultValue={props.formValueOne ? props.formValueOne : ""}
+                        />
+                        </div>
+                        <div className="col">
+                        <label htmlFor={props.htmlForTwo}>{props.formLabelTwo}</label>
+                        <textarea
+                            className="textarea"
+                            required
+                            rows="4" cols="30"
+                            placeholder={props.formPlaceholderTwo}
+                            name={props.formNameTwo}
+                            type="text"
+                            onChange={props.changeHandler}
+                            defaultValue={props.formValueTwo ? props.formValueTwo : ""}
+                        />
+                        </div>
+                        </div>
+                        <br />
+                            <div className="row">
+                        <CancelButton cancelHandler={props.cancelHandler} />
+                        <SubmitButton />
                     </div>
-                    <div className="col">
-                    <label htmlFor={props.htmlForTwo}>{props.formLabelTwo}</label>
-                    <textarea
-                        required
-                        rows="4" cols="30"
-                        placeholder={props.formPlaceholderTwo}
-                        name={props.formNameTwo}
-                        type="text"
-                        onChange={props.changeHandler}
-                        defaultValue={props.formValueTwo ? props.formValueTwo : ""}
-                    />
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2 className="p-2">{props.formTitle}</h2>
+                <form onSubmit={props.submitHandler}>
+                    <div className="row">
+                        <div className="col">
+                        <label htmlFor={props.htmlForOne}>{props.formLabelOne}</label>
+                        <textarea
+                            className="textarea"
+                            required
+                            rows="4" cols="30"
+                            placeholder={props.formPlaceholderOne}
+                            name={props.formNameOne}
+                            onChange={props.changeHandler}
+                            defaultValue={props.formValueOne ? props.formValueOne : ""}
+                        />
+                        </div>
+                        <div className="col">
+                        <label htmlFor={props.htmlForTwo}>{props.formLabelTwo}</label>
+                        <textarea
+                            className="textarea"
+                            required
+                            rows="4" cols="30"
+                            placeholder={props.formPlaceholderTwo}
+                            name={props.formNameTwo}
+                            onChange={props.changeHandler}
+                            defaultValue={props.formValueTwo ? props.formValueTwo : ""}
+                        />
+                        </div>
+                        </div>
+                        <br />
+                            <div className="row">
+                        <CancelButton cancelHandler={props.cancelHandler} />
+                        <SubmitButton />
                     </div>
-                    </div>
-                    <br />
-                        <div className="row">
-                    <CancelButton cancelHandler={props.cancelHandler} />
-                    <SubmitButton />
-                </div>
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
+    }
 };
 
 export default Form;
